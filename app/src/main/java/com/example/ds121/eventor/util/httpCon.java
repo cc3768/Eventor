f2fd2b02b29feeb918ed7b32c82561690c31e3d6
Alexander Feathers <fang_shadow@echonetwork.us>
2017-07-10 17:21:01 -0400

    Trying to get httpCon working for the api connection.

---
+++ app/src/main/java/com/example/ds121/eventor/util/httpCon.java
@@ -0,0 +1,17 @@
+package com.example.ds121.eventor.util;
+
+import android.os.AsyncTask;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+
+
+/**
+ * Created by Alexander on 7/10/2017.
+ */
+
+public class httpCon extends AsyncTask {
+    protected String doInBackground(Void... params){
+        HttpClient httpClient = new DefaultHttpClient();
+
+    }
+}
